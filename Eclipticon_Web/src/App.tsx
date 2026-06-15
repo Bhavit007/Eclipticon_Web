795c94495213b3b0c78b2d16fc14c3342f55e025
Bhavit007 <bhavit.choudhary@eclipticon.com>
2026-06-15 11:58:53 +0530

    [UPDATE] Explore button CSS

--- Eclipticon_Web/src/App.tsx
+++ Eclipticon_Web/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Button from "./Components/BlogButton";
+import Explore_button from "./Components/Explore_button";
 
 function App() {
   return (
     <div className="App">
       <>
         <Button />
+      </>
+      <>
+        <Explore_button />
       </>
     </div>
   );
