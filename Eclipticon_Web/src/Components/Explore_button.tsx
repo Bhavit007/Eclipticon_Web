7d800876c1192982897e4deb70bcf2cbd2a01292
Bhavit007 <bhavit.choudhary@eclipticon.com>
2026-06-16 17:11:00 +0530

    [ADD] Hero and its css

--- Eclipticon_Web/src/Components/Explore_button.tsx
+++ Eclipticon_Web/src/Components/Explore_button.tsx
@@ -1,11 +1,11 @@
 import styles from "./Explore_button.module.css";
 
-const Explore_button = () => {
+const Explore_button = (text: string) => {
   return (
     <div>
       <button className={styles.button}>
         <button className={styles.button__inner}>
-          <p className={styles.button__text}>Explore Services </p>
+          <p className={styles.button__text}>{text}</p>
         </button>
         <img src="/icon/ic-white-right-arrow.svg" />
       </button>
---
+++ Eclipticon_Web/src/Components/Hero.tsx
@@ -0,0 +1,20 @@
+import styles from "./Hero.module.css";
+import Explore_button from "./Explore_button";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1 className={styles["hero__title"]}>
+        Stories, breakdowns & behind-the-scenes from our product lab.
+      </h1>
+      <p className={styles["hero__description"]}>
+        We don’t blog for clicks. We write to document our thinking, process,
+        and the small wins (and mistakes) that make great digital products
+        possible.
+      </p>
+      <div>{Explore_button("Get Started")}</div>
+    </div>
+  );
+};
+
+export default Hero;
